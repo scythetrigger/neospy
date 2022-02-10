f8847b030ff5708b38f5516fd261ed83868c8fbd
Nicholas Parham <62166636+scythetrigger@users.noreply.github.com>
2022-02-10 01:46:49 -0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -10,19 +10,11 @@ setup(
     license = 'Apache Software License',
     packages = ['neos'],
     install_requires = [
+        'base64',
         'bs4', 
         'requests',
+        'amplParser'
         ],
 
-    classifiers=[
-        'Development Status :: 1 - Planning',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',  
-        'Operating System :: POSIX :: Linux',        
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-    ],
+    classifiers = [],
 )
